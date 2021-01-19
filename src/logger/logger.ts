cb5f030c512cd482a72a1ee9dc283e839af27482
vip529 <b170188@nitsikkim.ac.in>
2021-01-19 20:37:24 +0530

    log level configuration

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -2,38 +2,71 @@ import { useTrackMetric } from '@microsoft/applicationinsights-react-js';
 import { SeverityLevel } from '@microsoft/applicationinsights-web';
 import { appInsights, reactPlugin } from './appinsight.config';
 
+// Utility function to  return a numerical equivalent of LogLevels
+
+const getLogLevel = () => {
+  const logLevel = process.env.REACT_APP_LOG_LEVEL;
+  if (logLevel === 'info') {
+    return 2;
+  }
+  if (logLevel === 'warn') {
+    return 3;
+  }
+  if (logLevel === 'error') {
+    return 4;
+  }
+  if (logLevel === 'silent') {
+    return 5;
+  }
+  return 1;
+};
+
 // Centralized log handler. Can plug any third party remote logging service here.
 
 const logger = (() => {
-  // use to log warnings
-  const logWarning = (message: string) => {
-    appInsights.trackTrace({
-      severityLevel: SeverityLevel.Warning,
-      message,
-    });
-  };
+  const logLevel = getLogLevel();
+
   // use to log information
   const logInfo = (message: string, properties: { [key: string]: any }) => {
-    appInsights.trackTrace({
-      severityLevel: SeverityLevel.Information,
-      message,
-      properties,
-    });
+    if (logLevel <= 2) {
+      appInsights.trackTrace({
+        severityLevel: SeverityLevel.Information,
+        message,
+        properties,
+      });
+    }
   };
+
+  // use to log warnings
+  const logWarning = (message: string) => {
+    if (logLevel <= 3) {
+      appInsights.trackTrace({
+        severityLevel: SeverityLevel.Warning,
+        message,
+      });
+    }
+  };
+
   // use to log errors
   const logError = (error: Error) => {
-    appInsights.trackException({
-      severityLevel: SeverityLevel.Error,
-      exception: error,
-    });
+    if (logLevel <= 4) {
+      appInsights.trackException({
+        severityLevel: SeverityLevel.Error,
+        exception: error,
+      });
+    }
   };
+
   // use to log application interaction/events
   const logAppEvent = (name: string, properties?: { [key: string]: any }) => {
-    appInsights.trackEvent({
-      name,
-      properties,
-    });
+    if (logLevel <= 5) {
+      appInsights.trackEvent({
+        name,
+        properties,
+      });
+    }
   };
+
   // use to log custom app metrics
   const logAppMetric = (name: string) => {
     const trackComponent = useTrackMetric(reactPlugin, name);
